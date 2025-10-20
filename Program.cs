0175251bdc5654f6ec14b76292354ccf291f5d76
jozo <ezudbsf@gmail.com>
2025-10-20 09:45:22 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+ class Program
+ {
+    static void Main(string[] args )
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone Nokia = new Nokia("1199999999", "Nokia Tijolao", "123456789", 32 );
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n ---------------------\n");
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone Iphone = new Iphone("11888888888", "Iphone 15", "987654321", 25);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Tekegram");
+
+    }
+ }
